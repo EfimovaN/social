a0f4eaab7edde90a27a7aaacb1d68371ebdea8d8
Nata <ef.nata@gmail.com>
2021-07-08 18:37:47 +0300

    Убираем side effect

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -5,24 +5,26 @@ import userPhoto from '../../img/user-icons.png';
 
 let Users = (props) => {
 
-  if (props.users.length === 0) {
+  let getUsers = () => {
+    if (props.users.length === 0) {
 
-    axios.get('https://social-network.samuraijs.com/api/1.0/users')
-      .then(response => {
-        props.setUsers(response.data.items);
-    });
-
-
-    // props.setUsers([
-    //   {id: 1, photoUrl: 'https://avatars.mds.yandex.net/get-zen_doc/1641332/pub_5d0764b7e75e750e0c9f8c86_5d076537789be40d64bb3e55/scale_1200', followed: false, fullName: 'Dmitry K.', status: 'I am looking for a Job right now', location: {country: 'Belarus', city: 'Minsk'}},
-    //   {id: 2, photoUrl: 'https://sun9-67.userapi.com/c851420/v851420113/17b609/2xTFgEyYQcM.jpg', followed: true, fullName: 'Maria T.', status: 'I am ', location: {country: 'Russia', city: 'Moscow'}},
-    //   {id: 3, photoUrl: 'https://proprikol.ru/wp-content/uploads/2019/07/prikolnye-kartinki-na-avu-21.jpg', followed: false, fullName: 'Tom S.', status: 'I am busy', location: {country: 'USA', city: 'Toronto'}}
-    //   ]
-    // );
-  }
+      axios.get('https://social-network.samuraijs.com/api/1.0/users')
+        .then(response => {
+          props.setUsers(response.data.items);
+      });
   
+      // props.setUsers([
+      //   {id: 1, photoUrl: 'https://avatars.mds.yandex.net/get-zen_doc/1641332/pub_5d0764b7e75e750e0c9f8c86_5d076537789be40d64bb3e55/scale_1200', followed: false, fullName: 'Dmitry K.', status: 'I am looking for a Job right now', location: {country: 'Belarus', city: 'Minsk'}},
+      //   {id: 2, photoUrl: 'https://sun9-67.userapi.com/c851420/v851420113/17b609/2xTFgEyYQcM.jpg', followed: true, fullName: 'Maria T.', status: 'I am ', location: {country: 'Russia', city: 'Moscow'}},
+      //   {id: 3, photoUrl: 'https://proprikol.ru/wp-content/uploads/2019/07/prikolnye-kartinki-na-avu-21.jpg', followed: false, fullName: 'Tom S.', status: 'I am busy', location: {country: 'USA', city: 'Toronto'}}
+      //   ]
+      // );
+    }
+  }
+
 
   return <div  >
+      <button onClick={getUsers}>Get users</button>
       { 
         props.users.map( u => <div key={u.id} className={styles.container}>
             <div className={styles.columnLeft}>
