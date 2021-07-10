9eb70f90c68cd0589c25fef852157147d2e74286
Nata <ef.nata@gmail.com>
2021-07-10 13:11:20 +0300

    Preloader

--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -3,13 +3,15 @@ const UNFOLLOW = 'UNFOLLOW';
 const SET_USERS = 'SET_USERS';
 const SET_TOTAL_USERS_COUNT = 'SET_TOTAL_USERS_COUNT';
 const SET_CURRENT_PAGE = 'SET_CURRENT_PAGE';
+const TOGGLE_IS_FETCHING = 'TOGGLE_IS_FETCHING';
 
 
 let initialState = {
   users: [],
   pageSize: 10,
   totalUsersCount: 0,
-  currentPage: 1
+  currentPage: 1,
+  isFetching: true
 }
 
 const usersReducer = (state = initialState, action) => {
@@ -60,6 +62,13 @@ const usersReducer = (state = initialState, action) => {
         currentPage: action.currentPage
       };
     }
+
+    case TOGGLE_IS_FETCHING: {
+      return {
+        ...state,
+        isFetching: action.isFetching
+      };
+    }
     
     default: 
       return state;
@@ -84,6 +93,10 @@ export const setTotalUsersCountAC = (totalUsersCount) => {
 
 export const setCurrentPageAC= (currentPage) => {
   return { type: SET_CURRENT_PAGE, currentPage}
+};
+
+export const toggleIsFetchingAC= (isFetching) => {
+  return { type: TOGGLE_IS_FETCHING, isFetching}
 };
 
 export default usersReducer;
