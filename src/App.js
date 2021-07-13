40e22c629792956a4df6ea4605032acc60792b7a
Nata <ef.nata@gmail.com>
2021-07-13 18:15:12 +0300

    cookie, login, auth

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header/Header';
-import Navbar from './components/Navbar/Navbar';
 import News from './components/News/News';
 import Music from './components/Music/Music';
 import Settings from './components/Settings/Settings';
@@ -9,15 +7,15 @@ import { Route } from 'react-router-dom';
 import MessagesContainer from './components/Messages/MessagesContainer';
 import UsersContainer from './components/Users/usersContainer';
 import ProfileContainer from './components/Profile/ProfileContainer';
+import HeaderContainer from './components/Header/HeaderContainer';
 
 
 const App = (props) => {
   return (
       <div className='app-wrapper'>
-        <Header />
-        <Navbar />
+        <HeaderContainer/>
         <div className='app-wrapper-content'>
-          <Route path='/profile/:userId' render={ () => <ProfileContainer />} />
+          <Route path='/profile/:userId?' render={ () => <ProfileContainer />} />
           <Route path='/messages' render={ () => <MessagesContainer />} />
           <Route path='/users' render={ () => <UsersContainer /> } />
           <Route path='/news' render={ () => <News />} />
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import Navbar from './Navbar/Navbar';
 import classes from './Header.module.css'
 
-const Header = () => {
+const Header = (props) => {
   return (
       <header className={classes.header}>
-        <p>SOCIAL</p>
+        <p className={classes.logo}>SOCIAL</p>
+       
+       <Navbar />
+        
+        <div className={classes.loginBlock}>
+          { props.isAuth ? props.login 
+            : <NavLink to={'/login'}>Login</NavLink>}
+        </div>
       </header>
   );
 }
