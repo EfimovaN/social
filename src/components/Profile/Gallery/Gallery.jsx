c4feca691b53324821f5b33a86c58a51ccdbb022
Nata <ef.nata@gmail.com>
2021-09-30 17:04:13 +0300

    Profile video

--- src/components/Profile/Gallery/Gallery.jsx
+++ src/components/Profile/Gallery/Gallery.jsx
@@ -16,6 +16,37 @@ const Gallery = () => {
                 </ul>
             </div>
 
+            <div className={classes.video}>
+                <h3>Video</h3>
+                <ul className={classes.video__list}>
+                    <li className={classes.video__item}>
+                        <iframe width="140" height="120" src="https://www.youtube.com/embed/tAgVINdc_o0"
+                                title="YouTube video player" frameBorder="0"
+                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                allowFullScreen></iframe>
+
+                    </li>
+                    <li className={classes.video__item}>
+                        <iframe width="140" height="120" src="https://www.youtube.com/embed/GNrdg3PzpJQ"
+                                title="YouTube video player" frameBorder="0"
+                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                allowFullScreen></iframe>
+                    </li>
+                    <li className={classes.video__item}>
+                        <iframe width="140" height="120" src="https://www.youtube.com/embed/0AohM_oOjBc"
+                                title="YouTube video player" frameBorder="0"
+                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                allowFullScreen></iframe>
+                    </li>
+                    <li className={classes.video__item}>
+                        <iframe width="140" height="120" src="https://www.youtube.com/embed/yDSs80lu3ak"
+                                title="YouTube video player" frameBorder="0"
+                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                allowFullScreen></iframe>
+                    </li>
+                </ul>
+            </div>
+
             <div className={classes.members}>
                 <h3>Latest Members</h3>
                 <ul className={classes.members__list}>
